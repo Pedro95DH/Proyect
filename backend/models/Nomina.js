84abd19db288cd8bfbe0b87b3d647693df888900
Pedro95DH <pedrosdhdez@gmail.com>
2026-03-07 10:04:00 +0000

    Modelos: Definición del esquema de Nóminas

---
+++ backend/models/Nomina.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const nominaSchema = new mongoose.Schema(
+  {
+    usuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+      required: true,
+    },
+    mes: {
+      type: Number,
+      required: true, 
+      min: 1,
+      max: 12,
+    },
+    anio: {
+      type: Number,
+      required: true,
+    },
+    rutaArchivo: { 
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Nomina", nominaSchema);
+
+    
